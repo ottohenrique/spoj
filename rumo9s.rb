c21eece9c405ca5aa66c9a09e387c9d897b8bdb1
Otto Henrique <ohorvath@sp.r7.com>
2011-08-03 17:37:15 -0300

    rumo9s | passou teste proposto

--- rumo9s.rb
+++ rumo9s.rb
@@ -1,7 +1,21 @@
+def soma_n n
+  s = 0
+  n.to_s.each_char do |i|
+    s = s + i.to_i
+  end
+  return s
+end
+
 STDIN.read.split("\n").each do |a|
   num = a.to_i
   if num > 0
-    print num
+    s = soma_n num
+
+    if((s % 9) == 0)
+      puts "#{num} is a multiple of 9 and has 9-degree #{s.to_s.size}"
+    else 
+      puts "#{num} is not a multiple of 9"
+    end
   else
     exit 0
   end
