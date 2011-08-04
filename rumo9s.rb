43a6d83f3fab6f804c2331ca0e72efac5faeb790
Otto Henrique <ohorvath@sp.r7.com>
2011-08-04 16:24:41 -0300

    retirado typecast para acelerar calc

--- rumo9s.rb
+++ rumo9s.rb
@@ -1,16 +1,22 @@
 STDIN.read.split("\n").each do |a|
-  if a != '0'
-    s=0
-    a.each_char do |c|
-        s = s + c.to_i
+    if a != '0'
+        nr = a.to_i
+        soma = 0
+
+        while nr > 0
+            n = nr % 10
+            soma += n
+            nr = (nr - n) / 10
+        end
+        
+        if((soma % 9) == 0)
+          puts "#{a} is a multiple of 9 and has 9-degree #{soma.to_s.size}"
+        else 
+         puts "#{a} is not a multiple of 9"
+        end
+    else
+        exit 0
     end
-    
-    if((s % 9) == 0)
-      puts "#{a} is a multiple of 9 and has 9-degree #{s.to_s.size}"
-    else 
-      puts "#{a} is not a multiple of 9"
-    end
-  else
-    exit 0
-  end
 end
+
+
