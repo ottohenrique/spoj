c0b32e7c531dd42edabd7136c9a02335ed20c6d3
Otto Henrique <ohorvath@sp.r7.com>
2011-08-18 15:49:40 -0300

    rumo9s/ usando variaveis padrao in-out

--- rumo9s.rb
+++ rumo9s.rb
@@ -1,17 +1,15 @@
-while ((nr=gets.to_i)!=0)
-    l = nr
+while $stdin.readline.chomp != '0'
+    nr = $_.to_i
     ct = (nr == 9) ? 1 : 0
 
     while nr > 9
         nr = nr.to_s.split('').inject(0) {|s, c| s + c.to_i}
         ct += 1
-        
-        puts "#{ct} soma: #{nr}"
     end   
             
     if(nr == 9)
-        puts "#{l} is a multiple of 9 and has 9-degree #{ct}"
+        puts "#{$_.chomp} is a multiple of 9 and has 9-degree #{ct}"
     else 
-        puts "#{l} is not a multiple of 9"
+        puts "#{$_.chomp} is not a multiple of 9"
     end
 end
