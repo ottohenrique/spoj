98a3c8058946e255e4645d5a7ed8135f287ff8c0
Otto Henrique <ohorvath@sp.r7.com>
2011-09-12 14:25:57 -0300

    .

--- rumo9s.rb
+++ rumo9s.rb
@@ -1,4 +1,6 @@
-while $stdin.readline.chomp != '0'
+resp = []
+
+while gets()
     nr = $_.to_i
     ct = (nr == 9) ? 1 : 0
 
@@ -8,8 +10,12 @@ while $stdin.readline.chomp != '0'
     end   
             
     if(nr == 9)
-        puts "#{$_.chomp} is a multiple of 9 and has 9-degree #{ct}"
+        resp << "#{$_.chomp} is a multiple of 9 and has 9-degree #{ct}"
     else 
-        puts "#{$_.chomp} is not a multiple of 9"
+        resp << "#{$_.chomp} is not a multiple of 9"
     end
+end
+
+resp.each do |s|
+  puts s
 end
